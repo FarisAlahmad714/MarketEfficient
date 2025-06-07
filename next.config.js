2eb935034954afa168644a22f73f974f28d480f7
FarisAlahmad714 <Farisalahmad01@hotmail.com>
2025-06-06 20:35:41 -0700

    Wrapping up for testing deployment enviroment for brute self and beta testing , will add a few more features like api switches and minor touch ups

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 // next.config.js
-module.exports = {
+const { withSentryConfig } = require('@sentry/nextjs');
+
+const nextConfig = {
   reactStrictMode: true,
 
   // Security headers
@@ -36,7 +38,7 @@ module.exports = {
                 "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com; " +
                 "font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com; " +
                 "img-src 'self' data: https: blob:; " +
-                "connect-src 'self' https://api.coingecko.com https://www.alphavantage.co https://firebase.googleapis.com https://firebaseinstallations.googleapis.com https://www.google-analytics.com; " +
+                "connect-src 'self' https://api.coingecko.com https://www.alphavantage.co https://firebase.googleapis.com https://firebaseinstallations.googleapis.com https://www.google-analytics.com https://o4509453821804544.ingest.us.sentry.io https://*.sentry.io; " +
                 "frame-ancestors 'none'; " +
                 "base-uri 'self'; " +
                 "form-action 'self'; " +
@@ -46,7 +48,7 @@ module.exports = {
                 "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com; " +
                 "font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com; " +
                 "img-src 'self' data: https: blob:; " +
-                "connect-src 'self' https://api.coingecko.com https://www.alphavantage.co https://firebase.googleapis.com https://firebaseinstallations.googleapis.com https://www.google-analytics.com; " +
+                "connect-src 'self' https://api.coingecko.com https://www.alphavantage.co https://firebase.googleapis.com https://firebaseinstallations.googleapis.com https://www.google-analytics.com https://o4509453821804544.ingest.us.sentry.io https://*.sentry.io; " +
                 "frame-ancestors 'none'; " +
                 "base-uri 'self'; " +
                 "form-action 'self'; " +
@@ -93,3 +95,38 @@ module.exports = {
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
+
+module.exports = withSentryConfig(nextConfig, {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
+
+  org: "chartsense",
+  project: "javascript-nextjs",
+
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  // tunnelRoute: "/monitoring",
+
+  // Hides source maps from generated client bundles
+  hideSourceMaps: true,
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+
+  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+  // See the following for more information:
+  // https://docs.sentry.io/product/crons/
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/instrumentation/automatic-instrumentation/
+  // automaticVercelMonitors: true,
+});
